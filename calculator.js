ccfd8dfe28bb84dfa445025546d3d26cc17ed2e9
lenixbyte <priyankabhardwaj065@gmail.com>
2023-02-10 08:00:44 +0530

    added simple-interest

--- calculator.js
+++ calculator.js
@@ -4,5 +4,8 @@ const compoundInterest = (principal, rate, time) => {
     return Math.floor(principal * (1 + rate) ** time);
     }
 
-module.exports = compoundInterest;
+const simpleInterest = (principal, rate, time) => {
+    return (principal * rate * time)/100;
+}
 
+module.exports = {compoundInterest, simpleInterest};
