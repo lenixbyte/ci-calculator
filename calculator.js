6306b6df7f4263b0660cfee32c767dfa3cd128ba
lenixbyte <priyankabhardwaj065@gmail.com>
2023-02-10 07:45:02 +0530

    added wrong code in calculator.js

--- calculator.js
+++ calculator.js
@@ -1,7 +1,7 @@
 // compound interest calculator
 
 const compoundInterest = (principal, rate, time) => {
-    return Math.floor(principal * (1 + rate) ** time);
+    return Math.floor(principal * (1 + time) ** rate);
     }
 
 module.exports = compoundInterest;
